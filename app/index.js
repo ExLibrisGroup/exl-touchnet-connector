54b05531382877b0fbe94bda088835961c342386
Josh Weisman <josh.m.weisman@gmail.com>
2020-10-21 14:41:42 +0300

    Refactor to remove duplication

--- app/index.js
+++ app/index.js
@@ -9,6 +9,7 @@ const { requestp } = require('./utils');
 const { frombase64 } = require('./utils');
 const fs = require('fs');
 
+/* global URL */
 nconf.env().file('config', './config.json');
 
 const http = require('http');
@@ -21,7 +22,12 @@ if (process.env.CERTIFICATE_KEY_FILE) {
 }
 
 let touchnet;
-TouchnetWS.init(nconf.get('TOUCHNET_WS_URL')).then(t=>touchnet=t);
+const init = async () => {
+  if (!touchnet) {
+    console.log('Initializing Touchnet');
+    touchnet = await TouchnetWS.init(nconf.get('TOUCHNET_WS_URL'));
+  }
+}
 
 const PORT = process.env.PORT || 3002;
 const SECURE_PORT = process.env.SECURE_PORT || 3003;
@@ -36,25 +42,35 @@ app.get('/touchnet', async (request, response) => {
   const returnUrl = (request.protocol + '://' + request.get('host') + request.originalUrl.split("?").shift()).replace(/\/$/, "");;
   const referrer = request.header('Referer');
 
-  let user_id, total_sum, upay_site_id, upay_site_url, post_message = '';
-  if (request.query.s) { 
+  try {
+    const resp = await get(request.query, returnUrl, referrer);
+    response.send(resp);
+  } catch(e) {
+    return response.status(400).send(e.message);
+  }
+})
+
+const get = async (qs, returnUrl, referrer) => {
+  await init();
+  let user_id, total_sum, upay_site_id, upay_site_url, post_message = 'false';
+  if (qs.s) { 
     /* From CloudApp */
-    ({ user_id, total_sum, upay_site_id, upay_site_url } = JSON.parse(frombase64(request.query.s)));
+    ({ user_id, total_sum, upay_site_id, upay_site_url } = JSON.parse(frombase64(qs.s)));
     post_message = 'true';
-  } else if (request.query.jwt) { 
+  } else if (qs.jwt) { 
     /* From Primo VE */
     try {
-      user_id = jwt.decode(request.query.jwt).userName;
+      user_id = jwt.decode(qs.jwt).userName;
       ({ total_sum } = await alma.getp(`/users/${user_id}/fees`));
     } catch (e) {
       console.error("Error in retrieving user information:", e.message)
-      return response.status(400).send('Cannot retrieve user details information.');
+      throw new Error('Cannot retrieve user details information.');
     }
-  } else if (request.query.pds_handle) {
+  } else if (qs.pds_handle) {
     /* From Primo Classic */
     try {
       const ref = new URL(referrer);
-      const url = `${ref.protocol}//${ref.host}/primo_library/libweb/webservices/rest/PDSUserInfo?institute=${request.query.institution}&pds_handle=${request.query.pds_handle}`;
+      const url = `${ref.protocol}//${ref.host}/primo_library/libweb/webservices/rest/PDSUserInfo?institute=${qs.institution}&pds_handle=${qs.pds_handle}`;
       console.log('retrieving borinfo from', url);
       const borinfo = await requestp({url});
       const node = require('xpath').select('/bor/bor_id/id', new dom().parseFromString(borinfo));
@@ -62,11 +78,11 @@ app.get('/touchnet', async (request, response) => {
       ({ total_sum } = await alma.getp(`/users/${user_id}/fees`));
     } catch (e) {
       console.error("Error in retrieving user information:", e.message)
-      return response.status(400).send('Cannot retrieve user details information.');
+      throw new Error('Cannot retrieve user details information.');
     }
   }
 
-  if (!user_id || total_sum <= 0) return response.status(400).send('Nothing to pay');
+  if (!user_id || total_sum <= 0) throw new Error('Nothing to pay');
 
   try {
     let ticket = await touchnet.generateTicket(user_id, {
@@ -77,37 +93,47 @@ app.get('/touchnet', async (request, response) => {
       referrer: referrer,
       post_message: post_message
     });
-    response.send(responses.redirectForm(ticket, user_id, upay_site_id, upay_site_url))
+    console.log('Successfully created ticket', ticket);
+    return responses.redirectForm(ticket, user_id, upay_site_id, upay_site_url);
   } catch (e) {
     console.error("Error in setting up payment:", e.message)
-    return response.status(400).send('Cannot prepare payment information.');
+    throw new Error('Cannot prepare payment information.');
+  }
+}
+
+app.post('/touchnet/success', async (request, response) => {
+  try {
+    const resp = await success(request.body);
+    response.send(resp);
+  } catch(e) {
+    return response.status(400).send(e.message);
   }
 })
 
-app.post('/touchnet/success', async (request, response) => {
-  const amount = request.body.pmt_amt;
-
+const success = async body => {
+  await init();
+  const amount = body.pmt_amt;
   let receipt, user_id, referrer, post_message;
   try {
-    ({ receipt, user_id, referrer, post_message } = await touchnet.authorize(request.body.session_identifier));
+    ({ receipt, user_id, referrer, post_message } = await touchnet.authorize(body.session_identifier));
   } catch(e) {
     console.error("Error while authorizing payment:", e.message);
-    return response.status(400).send('Could not authorize payment.')
+    throw new Error('Could not authorize payment.')
   }
 
   try {
-    if (post_message) {
-      response.send(responses.returnToReferrer(referrer, { amount: amount, external_transaction_id: receipt, user_id: user_id }));
+    if (post_message === 'true') {
+      return responses.returnToReferrer(referrer, { amount: amount, external_transaction_id: receipt, user_id: user_id });
     } else {    
       await alma.postp(`/users/${user_id}/fees/all?op=pay&amount=${amount}&method=ONLINE&external_transaction_id=${receipt}`, null);
       console.log('Payment posted to Alma. Returning to referrer', referrer);
-      response.send(responses.returnToReferrer(referrer));
+      return responses.returnToReferrer(referrer);
     }
   } catch (e) {
     console.error("Error in posting payment to Alma:", e.message);
-    return response.status(400).send('Could not post payment to Alma')
+    throw new Error('Could not post payment to Alma')
   }
-})
+}
 
 app.get('/touchnet/error', (request, response) => {
   response.status(400).send('An error has occurred');
@@ -116,3 +142,5 @@ app.get('/touchnet/error', (request, response) => {
 // app.listen(PORT);
 http.createServer(app).listen(PORT);
 if (credentials) https.createServer(credentials, app).listen(SECURE_PORT);
+
+module.exports = { get, success };
--- app/lambda.js
+++ app/lambda.js
@@ -1,22 +1,11 @@
-const jwt     = require('jsonwebtoken');
-const alma    = require('almarestapi-lib');
-const nconf   = require('nconf');
 const { parse }       = require('querystring');
-const TouchnetWS      = require('./touchnet');
-const responses       = require('./responses');
-const dom = require('xmldom').DOMParser;
-const { requestp } = require('./utils');
-const { frombase64 }  = require('./utils');
-
-let touchnet;
-/* global URL */
+const { get, success } = require('./index');
 
 exports.handler = async (event, context) => {
-  touchnet = await TouchnetWS.init(nconf.get('TOUCHNET_WS_URL'));
   const { path, method } = event.requestContext.http;
   let response;
   if (path.startsWith('/touchnet/success') && method == 'POST') {
-    response = success(event);
+    response = showSuccess(event);
   } else if (path.startsWith('/touchnet/error')) {
     console.error("An error has occurred in the Touchnet process");
     return { statusCode: 400, body: 'An error has occurred in the Touchnet processing.' };
@@ -29,94 +18,30 @@ exports.handler = async (event, context) => {
 const connector = async (event) => {
   const returnUrl = `${event.headers['x-forwarded-proto']}://${event.headers.host}${event.requestContext.http.path}`.replace(/\/$/, "");
   const referrer = event.headers['referer'];
-  const queryStringParameters = event.queryStringParameters;
-
-  if (!queryStringParameters) return { statusCode: 400, body: 'No parameters provided.' }
-  let user_id, total_sum, upay_site_id, upay_site_url, post_message = '';
-  if (queryStringParameters.s) { 
-    /* From CloudApp */
-    ({ user_id, total_sum, upay_site_id, upay_site_url } = JSON.parse(frombase64(queryStringParameters.s)));
-    post_message = 'true';
-  } else if (queryStringParameters.jwt) { 
-    /* From Primo VE */
-    try {
-      user_id = jwt.decode(queryStringParameters.jwt).userName;
-      user_id = 'exl_impl';
-      ({ total_sum } = await alma.getp(`/users/${user_id}/fees`));
-    } catch (e) {
-      console.error("Error in retrieving user information:", e.message)
-      return { statusCode: 400, body: 'Cannot retrieve user details information.' };
-    } 
-  } else if (queryStringParameters.pds_handle) {
-    /* From Primo Classic */
-    try {
-      const ref = new URL(referrer);
-      const url = `${ref.protocol}//${ref.host}/primo_library/libweb/webservices/rest/PDSUserInfo?institute=${queryStringParameters.institution}&pds_handle=${queryStringParameters.pds_handle}`;
-      console.log('retrieving borinfo from', url);
-      const borinfo = await requestp({url});
-      const node = require('xpath').select('/bor/bor_id/id', new dom().parseFromString(borinfo));
-      user_id = node.length > 0 ? node[0].firstChild.data : null;
-      ({ total_sum } = await alma.getp(`/users/${user_id}/fees`));
-    } catch (e) {
-      console.error("Error in retrieving user information:", e.message)
-      return { statusCode: 400, body: 'Cannot retrieve user details information.' };
-    }
-  }
-
-  if (!user_id || total_sum <= 0) return { statusCode: 400, body: 'Nothing to pay.' };
 
   try {
-    let ticket = await touchnet.generateTicket(user_id, {
-      amount: total_sum,
-      success: returnUrl + '/success',
-      error: returnUrl + '/error',
-      cancel: referrer,
-      referrer: referrer,
-      post_message: post_message
-    });
-    console.log('Successfully created ticket', ticket);
+    const resp = await get(event.queryStringParameters, returnUrl, referrer);
     return { 
       statusCode: 200, 
       headers: { 'Content-type': 'text/html' },
-      body: responses.redirectForm(ticket, user_id, upay_site_id, upay_site_url)
+      body: resp
     };
-  } catch (e) {
-    console.error("Error in setting up payment:", e.message)
-    return  { statusCode: 400, body: 'Cannot prepare payment information.' };
-  }  
+  } catch(e) {
+    return { statusCode: 400, body: e.message };
+  }
 }
 
-const success = async (event) => {
+const showSuccess = async (event) => {
   const body = event.isBase64Encoded ? Buffer.from(event.body, 'base64').toString() : event.body;
-  const { pmt_amt, session_identifier } = parse(body);
-
-  let receipt, user_id, referrer, post_message;
   try {
-    ({ receipt, user_id, referrer, post_message } = await touchnet.authorize(session_identifier));
+    const resp = await success(parse(body));
+    return { 
+      statusCode: 200, 
+      headers: { 'Content-type': 'text/html' },
+      body: resp
+    }    
   } catch(e) {
-    console.error("Error while authorizing payment:", e.message);
-    return  { statusCode: 400, body: 'Could not authorize payment.' };
-  }
-
-  try {
-    if (post_message) {
-      return { 
-        statusCode: 200, 
-        headers: { 'Content-type': 'text/html' },
-        body: responses.returnToReferrer(referrer, { amount: pmt_amt, external_transaction_id: receipt, user_id: user_id })
-      }
-    } else {    
-      await alma.postp(`/users/${user_id}/fees/all?op=pay&amount=${pmt_amt}&method=ONLINE&external_transaction_id=${receipt}`, null);
-      console.log('Payment posted to Alma. Returning to referrer', referrer);
-      return { 
-        statusCode: 200, 
-        headers: { 'Content-type': 'text/html' },
-        body: responses.returnToReferrer(referrer)
-      }
-    }
-  } catch (e) {
-    console.error("Error in posting payment to Alma:", e.message);
-    return  { statusCode: 400, body: 'Could not post payment to Alma' };
+    return  { statusCode: 400, body: e.message };
   }
 }
 
